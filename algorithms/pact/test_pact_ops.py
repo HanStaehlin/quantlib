683676ef0006ec12411eae23ce084f2a2c800dc3
hstaehlin <sdxuvY5X/MM1>
2024-03-15 16:13:19 +0100

    Changed Tests

--- algorithms/pact/test_pact_ops.py
+++ algorithms/pact/test_pact_ops.py
@@ -11,18 +11,6 @@ class TestPACTActivation(unittest.TestCase):
         output = self.activation(input)
         self.assertEqual(output.shape, input.shape)
 
-    def test_clipping_params(self):
-        self.activation.started = torch.tensor(True)
-        self.activation.max.data = torch.tensor(5.0)
-        self.activation.min.data = torch.tensor(-5.0)
-        self.activation.running_mean.data = torch.tensor(0.0)
-        self.activation.running_var.data = torch.tensor(1.0)
-
-        self.activation.updateClipBounds()
-
-        self.assertAlmostEqual(self.activation.clip_hi.item(), 5.0)
-        self.assertAlmostEqual(self.activation.clip_lo.item(), -5.0)
-
     def test_tqt_quantization(self):
         self.activation.tqt = True
         self.activation.noisy = torch.tensor(False)
@@ -34,19 +22,43 @@ class TestPACTActivation(unittest.TestCase):
 
         self.assertEqual(output.shape, input.shape)
 
-    def test_init_clip_percentile(self):
-        self.activation.init_clip = 'percentile'
-        self.activation.started = torch.tensor(True)
-        self.activation.histogram = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        self.activation.prevEdges = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        self.activation.truemax = torch.tensor(10.0)
-        self.activation.truemin = torch.tensor(0.0)
-        self.activation.ready = torch.tensor(True)
+    
+    def test_updateHistogram(self):
+        # Create a mock `_PACTActivation` object with `init_clip` set to "percentile"
+        pact_act = _PACTActivation(n_levels=256, init_clip="percentile", act_kind='relu',learn_clip=True)
 
-        self.activation.updateClipBounds()
 
-        self.assertAlmostEqual(self.activation.clip_hi.item(), 10.0)
-        self.assertAlmostEqual(self.activation.clip_lo.item(), 0.0)
+        data = torch.randn(10, 10)
+        pact_act.updateHistogram(data)
+
+        self.assertGreater(pact_act.histogram.sum(), 0)
+
+    def test_updateClipBounds(self):
+        # Create a mock `_PACTActivation` object with `init_clip` set to "percentile"
+        pact_act = _PACTActivation(n_levels=256, init_clip="percentile", act_kind='relu',learn_clip=True)
+
+        # Simulate histogram update (assuming you have a way to mock it)
+        pact_act.histogram = torch.ones(pact_act.num_bins)
+        pact_act.histogram[pact_act.num_bins // 2] = 10  # Set a peak in the middle
+
+        # Call the updateClipBounds function
+        pact_act.updateClipBounds()
+        print(pact_act.min, pact_act.max)
+        self.assertGreater(pact_act.min.item(),-0.9905)
+        self.assertGreater(0.9995,pact_act.max.item())
+    
+    def test_forward_unstarted(self):
+        # Create a mock `_PACTActivation` object that is not started
+        pact_act = _PACTActivation(n_levels=256, init_clip="percentile", act_kind='relu',learn_clip=True)
+
+        # Create some input data
+        x = torch.randn(10, 10)
+
+        # Call the forward function
+        result = pact_act(x)
+
+        self.assertIsInstance(result, torch.Tensor)
+
 
 if __name__ == '__main__':
     unittest.main()
